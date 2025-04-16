3988d771e7e33cdd2120d5c4d42ca83683e72f20
bebehr <behr.benny@gmail.com>
2025-04-16 20:47:23 +0200

    Using faker in unit tests

--- tests/Example/GreeterTest.php
+++ tests/Example/GreeterTest.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Bebehr\TemplatePhpLibrary\Tests\Example;
 
 use Bebehr\TemplatePhpLibrary\Example\Greeter;
+use Faker\Factory;
+use Faker\Generator;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -12,12 +14,21 @@ use PHPUnit\Framework\TestCase;
  */
 final class GreeterTest extends TestCase
 {
+    private Generator $faker;
+
+    protected function setUp(): void
+    {
+        $this->faker = Factory::create();
+    }
+
     public function testGreetsWithName(): void
     {
         $greeter = new Greeter();
+        $name = $this->faker->name();
 
-        $greeting = $greeter->greet('Alice');
+        $greeting = $greeter->greet($name);
+        $expected = "Hello, {$name}!";
 
-        self::assertSame('Hello, Alice!', $greeting);
+        self::assertSame($expected, $greeting);
     }
 }
